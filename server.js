49b23f47e57f6fdec5f0f756e8c759dc1e2679c7
gurralavarun <gurrala.varun11@gmail.com>
2026-08-18 20:32:18 +0530

    configure cors for frontend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const sequelize = require("./src/config/database");
 
 const {
@@ -29,6 +30,7 @@ const paymentRoutes = require("./src/routes/paymentRoutes");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
